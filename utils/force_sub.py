4b80da505c69278d65f436c2dae6e41a3e6b22ca
pastele060-arch <pastele060@gmail.com>
2026-09-10 02:06:45 +0700

    Update force_sub.py

--- utils/force_sub.py
+++ utils/force_sub.py
@@ -4,14 +4,14 @@ from aiogram.exceptions import TelegramBadRequest, TelegramForbiddenError
 
 CHANNELS = [
     {
-        "id": -1003978483597,
+        "id": -1004441808999,
         "name": "Channel Update",
-        "url": "https://t.me/+CUAmQ49VFMw1MmRh",
+        "url": "https://t.me/+JUGpWnRLT8ljNGZk",
     },
     {
-        "id": -1004413314849,
-        "name": "Saluran Backup",
-        "url": "https://t.me/+0ddS3Ha4c2pkNmJl",
+        "id": -1003971846572,
+        "name": "Saluran Notification",
+        "url": "https://t.me/noticsaluran",
     },
 ]
 
